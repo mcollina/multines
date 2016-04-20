741f29c81894ed33cca0f5899bd5ec656224a61f
Matteo Collina <hello@matteocollina.com>
2016-04-20 08:12:03 +0200

    Support mqemitter on multiple instances.

--- multines.js
+++ multines.js
@@ -1,8 +1,43 @@
 'use strict'
 
+const mqemitter = require('mqemitter')
+
 function register (server, options, next) {
   server.dependency('nes')
-  setImmediate(next)
+
+  options.mq = options.mq || mqemitter()
+
+  const mq = options.mq
+
+  server.decorate('server', 'subscriptionFar', (path, options) => {
+    options = options || {}
+
+    const toWrap = options.onSubscribe || ((socket, path, params, next) => next())
+
+    options.onSubscribe = (socket, path, params, next) => {
+      toWrap(socket, path, params, (err) => {
+        if (err) {
+          return next(err)
+        }
+        mq.on(path, function (message, done) {
+          socket.publish(message.topic, message.body, done)
+        }, next)
+      })
+    }
+
+    server.subscription(path, options)
+  })
+
+  server.decorate('server', 'publishFar', (path, message) => {
+    options = options || {}
+
+    mq.emit({
+      topic: path,
+      body: message
+    })
+  })
+
+  next()
 }
 
 module.exports.register = register
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const mqemitter = require('mqemitter')
 const Code = require('code')
 const Lab = require('lab')
 const Hapi = require('hapi')
@@ -13,13 +14,14 @@ const beforeEach = lab.beforeEach
 const afterEach = lab.afterEach
 const expect = Code.expect
 
-function getServer () {
+function getServer (port) {
+  port = port || 3000
   const server = new Hapi.Server()
-  server.connection({ port: 3000 })
+  server.connection({ port: port })
   return server
 }
 
-function register (server, opts, cb) {
+function start (server, opts, cb) {
   if (typeof opts === 'function') {
     cb = opts
     opts = {}
@@ -30,42 +32,30 @@ function register (server, opts, cb) {
     options: opts
   }
 
-  server.register([Nes, plugin], cb)
+  server.register([Nes, plugin], (err) => {
+    if (err) {
+      return cb(err)
+    }
+
+    server.start(cb)
+  })
+
+  server.subscriptionFar('/echo')
+  server.route({
+    path: '/echo',
+    method: 'POST',
+    handler: (req, reply) => {
+      server.publishFar('/echo', req.payload)
+      reply(req.payload)
+    }
+  })
 
   return server
 }
 
-experiment('nes work as normal', () => {
-  let server
-
-  beforeEach((done) => {
-    server = register(getServer(), (err) => {
-      if (err) {
-        return done(err)
-      }
-
-      // starting the server, needed to connect via nes
-      server.start(done)
-    })
-
-    server.subscription('/echo')
-    server.route({
-      path: '/echo',
-      method: 'POST',
-      handler: (req, reply) => {
-        server.publish('/echo', req.payload)
-        reply(req.payload)
-      }
-    })
-  })
-
-  afterEach((done) => {
-    server.stop(done)
-    server = null
-  })
-
+function pubSubTest () {
   test('pub/sub', (done) => {
-    var client = new Nes.Client('ws://localhost:3000')
+    const client = new Nes.Client('ws://localhost:3000')
 
     client.connect((err) => {
       if (err) {
@@ -92,4 +82,97 @@ experiment('nes work as normal', () => {
       })
     })
   })
+}
+
+function scalablePubSubTest () {
+  test('scalable pub/sub', (done) => {
+    const client1 = new Nes.Client('ws://localhost:3000')
+    const client2 = new Nes.Client('ws://localhost:3001')
+
+    client1.connect((err) => {
+      if (err) {
+        return done(err)
+      }
+      client2.connect((err) => {
+        if (err) {
+          return done(err)
+        }
+
+        client1.subscribe('/echo', (message) => {
+          expect(message).to.deep.equal({ hello: 'world' })
+          done()
+        }, (err) => {
+          if (err) {
+            return done(err)
+          }
+
+          client2.request({
+            path: '/echo',
+            method: 'POST',
+            payload: { hello: 'world' }
+          }, (err) => {
+            if (err) {
+              return done(err)
+            }
+          })
+        })
+      })
+    })
+  })
+}
+
+experiment('nes work as normal', () => {
+  let server
+
+  beforeEach((done) => {
+    server = start(getServer(), done)
+  })
+
+  afterEach((done) => {
+    server.stop(done)
+    server = null
+  })
+
+  pubSubTest()
+})
+
+experiment('with shared mqemitter', () => {
+  let server1
+  let server2
+  let mq
+
+  beforeEach((done) => {
+    mq = mqemitter()
+
+    server1 = start(getServer(), {
+      mq: mq
+    }, function (err) {
+      if (err) {
+        return done(err)
+      }
+
+      server2 = start(getServer(3001), {
+        mq: mq
+      }, done)
+    })
+  })
+
+  afterEach((done) => {
+    server1.stop((err) => {
+      if (err) {
+        return done(err)
+      }
+      server2.stop((err) => {
+        if (err) {
+          return done(err)
+        }
+        mq.close(done)
+      })
+      server2 = null
+    })
+    server1 = null
+  })
+
+  pubSubTest()
+  scalablePubSubTest()
 })
